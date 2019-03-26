1ecb0707f02189b48d87b8c338fdc8f0c86bedca
jjj@dooban.cn <jjj@dooban.cn>
2019-03-26 09:45:11 +0800

    test：填列表并展示 ps：我也不知道这干嘛的

---
+++ 20180608/js/js.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $("#clear").click(function () {
+        $("input[type=text]").val("");
+    });
+
+    $("#next").click(function () {
+        var a = $(".t1 .flex-item:nth-child(1) input").val();
+        var b = $(".t1 .flex-item:nth-child(2) input").val();
+        var c = $(".t1 .flex-item:nth-child(3) input").val();
+        alert(a + "," + b + "," + c);
+
+
+
+    });
+
+
+
+
+});
